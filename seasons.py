a21c784482d20acc3fc3ebb2d1da96ec47cd0483
rayliu2525 <98187660+rayliu2525@users.noreply.github.com>
2022-06-07 13:53:24 -0400

    Add files via upload
    Codebuild Test Project

---
+++ seasons.py
@@ -0,0 +1,31 @@
+from datetime import date
+import re
+import sys
+import inflect
+
+
+def main():
+    print(life_minutes(input("Date of Birth: ")))
+
+def life_minutes(s):
+    if re.search("^[0-9]{4}-[0-9]{2}-[0-9]{2}$", s) == None:
+        sys.exit("Invalid date")
+
+    year, month, day = s.split("-")
+    year = int(year)
+    month = int(month)
+    day = int(day)
+
+    dob = date(year, month, day)
+    current_date = date.today()
+
+    delta = current_date - dob
+    minutes = round(delta.total_seconds() / 60)
+
+    engine_object = inflect.engine()
+    minutes_words = engine_object.number_to_words(minutes)
+
+    return f"{minutes_words} minutes".capitalize().replace(" and", "")
+
+if __name__ == "__main__":
+    main()
